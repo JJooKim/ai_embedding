949f217fbc1c01c4ac2e2b97cb7c8984fe7dbb6c
JJooKim <rladudwn0625@gmail.com>
2023-09-29 13:57:40 +0900

    watch code changed

--- gpio/wiring4segwatch.c
+++ gpio/wiring4segwatch.c
@@ -18,12 +18,15 @@
 
 // Wpi pin numbers
 char pins[8] = {PA, PB, PC, PD, PE, PF, PG, PDEC};
+char pindps[4] = {PDP1, PDP2, PDP3, PDP4};
 
 void clear_pin()
 {
     int i;
     for (i = 0; i < 8; i++)
         digitalWrite(pins[i], 1);
+    for (i = 0; i < 4; i++)
+        digitalWrite(pindps[i], 0);
 }
 
 void set_pin(int n)
@@ -38,6 +41,8 @@ void init_pin()
     int i;
     for (i = 0; i < 8; i++)
         pinMode(pins[i], OUTPUT);
+    for (i = 0; i < 4; i++)
+        pinMode(pindps[i], OUTPUT);
 }
 
 int main(void)
@@ -45,37 +50,39 @@ int main(void)
     wiringPiSetup();
     init_pin();
 
-    while (1) // Run continuously
+    while (1) // 무한 루프
     {
-        time_t rawTime;
+        time_t curr_time;
+        time(&curr_time)
+
+
         struct tm *timeInfo;
 
-        time(&rawTime);          // Get current time
-        timeInfo = localtime(&rawTime); // Convert to local time
+        timeInfo = localtime(&curr_time); // Convert to local time
 
         int hours = timeInfo->tm_hour;
         int minutes = timeInfo->tm_min;
 
         // Display the hours and minutes
         set_pin(hours / 10);       // Display tens digit of hours
-        digitalWrite(PDP1, 0);    // Enable the first digit (leftmost)
+        digitalWrite(PDP1, 1);    // Enable the first digit (leftmost)
         delay(5);                  // Delay to display the digit
-        digitalWrite(PDP1, 1);    // Disable the first digit
+        digitalWrite(PDP1, 0);    // Disable the first digit
 
         set_pin(hours % 10);       // Display ones digit of hours
-        digitalWrite(PDP2, 0);    // Enable the second digit
+        digitalWrite(PDP2, 1);    // Enable the second digit
         delay(5);                  // Delay to display the digit
-        digitalWrite(PDP2, 1);    // Disable the second digit
+        digitalWrite(PDP2, 0);    // Disable the second digit
 
         set_pin(minutes / 10);     // Display tens digit of minutes
-        digitalWrite(PDP3, 0);    // Enable the third digit
+        digitalWrite(PDP3, 1);    // Enable the third digit
         delay(5);                  // Delay to display the digit
-        digitalWrite(PDP3, 1);    // Disable the third digit
+        digitalWrite(PDP3, 0);    // Disable the third digit
 
         set_pin(minutes % 10);     // Display ones digit of minutes
-        digitalWrite(PDP4, 0);    // Enable the fourth digit (rightmost)
+        digitalWrite(PDP4, 1);    // Enable the fourth digit (rightmost)
         delay(5);                  // Delay to display the digit
-        digitalWrite(PDP4, 1);    // Disable the fourth digit
+        digitalWrite(PDP4, 0);    // Disable the fourth digit
 
         delay(1000); // Delay for one second
     }
