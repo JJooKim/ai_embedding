d04e89b5fa5f507fc26bd6a4fe2d50403c7cf400
JJooKim <rladudwn0625@gmail.com>
2023-09-29 14:55:52 +0900

    hihi

--- gpio/wiring4segwatch.c
+++ gpio/wiring4segwatch.c
@@ -16,6 +16,9 @@
 #define PDP3 24
 #define PDP4 25
 
+// for anode display
+char nums[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+
 // Wpi pin numbers
 char pins[8] = {PA, PB, PC, PD, PE, PF, PG, PDEC};
 char pindps[4] = {PDP1, PDP2, PDP3, PDP4};
@@ -29,11 +32,11 @@ void clear_pin()
         digitalWrite(pindps[i], 1);
 }
 
-void set_pin(int n)
+void set_pin (int n)
 {
-    int i;
-    for (i = 0; i < 8; i++)
-        digitalWrite(pins[i], (n >> i) & 0x1);
+  int i;
+  for (i = 0; i < 8; i++)
+      digitalWrite(pins[i], (nums[n] >> i) & 0x1);
 }
 
 void init_pin()
@@ -70,18 +73,18 @@ int main(void)
     // Display the hours and minutes
     
     clear_pin();
-    //digitalWrite(PDP2, 0);    // Enable the first digit (leftmost)
-    //digitalWrite(PDP3, 0);
-    //digitalWrite(PDP4, 0);
+    digitalWrite(PDP2, 0);    // Enable the first digit (leftmost)
+    digitalWrite(PDP3, 0);
+    digitalWrite(PDP4, 0);
     set_pin(hours / 10);       // Display tens digit of hours
-    delay(1000);                  // Delay to display the digit
+    delay(5);                  // Delay to display the digit
     
     clear_pin();
-    //digitalWrite(PDP1, 0);    // Enable the first digit (leftmost)
-    //digitalWrite(PDP3, 0);
-    //digitalWrite(PDP4, 0);
+    digitalWrite(PDP1, 0);    // Enable the first digit (leftmost)
+    digitalWrite(PDP3, 0);
+    digitalWrite(PDP4, 0);
     set_pin(hours % 10);       // Display tens digit of hours
-    delay(1000);                  // Delay to display the digit
+    delay(5);                  // Delay to display the digit
 
 
     clear_pin();
@@ -89,7 +92,7 @@ int main(void)
     digitalWrite(PDP2, 0);
     digitalWrite(PDP4, 0);
     set_pin(minutes / 10);       // Display tens digit of hours
-    delay(1000);                  // Delay to display the digit
+    delay(5);                  // Delay to display the digit
 
 
     clear_pin();
@@ -97,7 +100,7 @@ int main(void)
     digitalWrite(PDP2, 0);
     digitalWrite(PDP3, 0);
     set_pin(minutes / 10);       // Display tens digit of hours
-    delay(1000);                  // Delay to display the digit
+    delay(5);                  // Delay to display the digit
 
     delay(1000); // Delay for one second
 
