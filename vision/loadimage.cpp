269d0c8c3e36045b86b86be72992ed3d2f8d6f22
JJooKim <rladudwn0625@gmail.com>
2023-10-11 23:36:22 +0900

    fixed

--- vision/loadimage.cpp
+++ vision/loadimage.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 
 int main()
 {
-  Mat image = imread("sample.jpg", IMREAD_COLOR);
+  Mat image = imread("sample.png", IMREAD_COLOR);
   imshow("Load Image", image); /* "Load image" 창에 이미지 표시*/
   waitKey(0); /*사용자의 키 입력을 대기한다. */
   return 0;
