2a4602314955888c1c3bc5471bf5b7d0dc71fbc2
JJooKim <rladudwn0625@gmail.com>
2023-10-20 21:38:39 +0900

    "Joy Stick Lab to study SPI communication"

--- serial/joystick.c
+++ serial/joystick.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <wiringPiSPI.h>
+
 #define CS_MCP3208 10 // BCM_GPIO 8 ,24pin
 #define SPI_CHANNEL 0 // one slave SPI
 #define SPI_SPEED 1000000 // 1MHz
@@ -14,53 +15,36 @@ int read_mcp3208_adc(unsigned char adcChannel)
     buff[0] = 0x06|((adcChannel & 0x07) >> 2); //d2 on the last bit
     buff[1] = (adcChannel & 0x07) << 6; //d1 and d0 on the first two bits
     buff[2] = 0x00;
-    digitalWrite(CS_MCP3208, 0); // Low : CS Active
+    digitalWrite(CS_MCP3208, 0); // CS Active by making it low
     wiringPiSPIDataRW(SPI_CHANNEL, buff, 3);
+    //sending a request to MCP3208 with information of which channel data to send back 
+
+
+
+    //You get the data back inside the buffer(need to handle the received data a bit)
     buff[1] = 0x0f & buff[1];
     adcValue = ( buff[1] << 8) | buff[2];
+    //buff[1] << 8 shifts the bits in buff[1] 8 positions to the left. This effectively converts buff[1] into the high byte of a 16-bit value, with the four least significant bits being zeros.
+    //
     digitalWrite(CS_MCP3208, 1); // High : CS Inactive, finish read data
     return adcValue;
 }
 
-
-int main (void)
+int main(void)
 {
-    int adcChannel = 0;
-    int adcValue = 0;
+    int adcValX, adcValY;
     wiringPiSetup();
-    pinMode(CS_MCP3208, OUTPUT); //cs output , data input from Dout
-
-    if(wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED) == -1)
+    wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED);
+    pinMode(CS_MCP3208, OUTPUT);
+    while(1)
     {
-        printf ( "wiringPiSPISetup Failed: %s\n", strerror(errno));
-        return 1;
-    }
-
-    for(int i=0;;i++) {
-        //adcChannel = i%3;
-        adcChannel=1;
-        adcValue = read_mcp3208_adc(adcChannel);
-
-        printf("%u", adcValue); //테스트 코드 라인 나오는 값 확인 위해
-
-        switch (adcChannel) {
-            case 0 : if (adcValue>800) printf(" X axis turn right %u \n",adcValue);
-                else if(adcValue < 200) printf(" X axis turn left %u\n",adcValue);
-                else printf(" X no turn %u \n",adcValue);
-                break;
-            case 1 : if (adcValue>800) printf(" Y backward %u\n",adcValue);
-                else if(adcValue < 200) printf(" Y forward %u \n",adcValue);
-                else printf(" Y no change %u \n",adcValue);
-                break;
-            case 2 : if (adcValue>300) printf(" SW OFF %u\n",adcValue);
-                else if(adcValue < 50) printf(" SW ON %u \n",adcValue);
-                else printf(" SW no change %u \n",adcValue);
-                break;
-            default : printf(" light Value i=%d = %u\n",adcChannel, adcValue);
-            //break;
-        }
-        //printf("adc0 Value i=%d = %u\n",adcChannel, adcValue);
-        delay(500) ; //ms
+        adcValX = read_mcp3208_adc(0);
+        adcValY = read_mcp3208_adc(1);
+        printf("X-axis: %u, Y-axis: %u\n", adcValX, adcValY);
+        delay(500);
     }
     return 0;
 }
+
+
+
